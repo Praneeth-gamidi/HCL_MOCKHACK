dab4e1e632be55aeddc94e76808c4153efab57e1
Praneeth-gamidi <gamidinvdpraneeth@gmail.com>
2026-04-20 12:40:47 +0530

    proper entity

---
+++ Backend/src/main/java/com/example/MPRRT/entity/Location.java
@@ -0,0 +1,19 @@
+package com.example.MPRRT.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data; 
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+@Entity
+@Table(name = "locations")
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ Backend/src/main/java/com/example/MPRRT/entity/PotholeReport.java
@@ -0,0 +1,20 @@
+package com.example.MPRRT.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data; 
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+@Entity
+@Table(name = "pothole_reports")
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+public class PotholeReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
+

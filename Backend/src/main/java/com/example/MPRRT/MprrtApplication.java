27364cc2290352b5dd62bcd2a94e37307111c430
Praneeth-gamidi <gamidinvdpraneeth@gmail.com>
2026-04-20 11:42:08 +0530

    First Commit

---
+++ Backend/src/main/java/com/example/MPRRT/MprrtApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPRRT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprrtApplication.class, args);
+	}
+
+}
